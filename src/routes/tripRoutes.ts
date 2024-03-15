e92082c23d2ee3b4e54edfb11a18cd66aac01a21
MariusMokymai <testams752@gmail.com>
2024-03-15 10:43:36 +0200

    post works

--- src/routes/tripRoutes.ts
+++ src/routes/tripRoutes.ts
@@ -59,23 +59,76 @@ tripsRouter.get('/:tripId', async (req, res) => {
 tripsRouter.post('/', async (req, res) => {
   // tai kas atsiusta gyvena ?
 
-  const { name, date, country, city, rating, description, price, user_id, image_main } =
-    req.body as Omit<TripObjType, 'id'>;
+  const {
+    name,
+    date,
+    country,
+    city,
+    rating,
+    description,
+    price,
+    user_id,
+    image_main,
+    images_1 = '',
+    images_2 = '',
+    images_3 = '',
+  } = req.body as Omit<TripObjType, 'id'>;
   // validation
-  const argArr = [name, date, country, city, rating, description, price, user_id, image_main];
+  const argArr = [
+    name,
+    date,
+    country,
+    city,
+    rating,
+    description,
+    price,
+    user_id,
+    image_main,
+    images_1,
+    images_2,
+    images_3,
+  ];
 
   // panaudoti dbQueryWithData ir sukurti nauja irasa
+  const sql = `INSERT INTO trips (name, date, country, city, rating, description, price, user_id, image_main, images_1, images_2, images_3) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`;
 
   // jei sekmingai sukurta grzinti naujai sukurto iraso id
+  const [rows, error] = (await dbQueryWithData(sql, argArr)) as [ResultSetHeader, Error];
+
   // grazinti pilna nauja objekta
+  if (error) {
+    console.warn('sukurti nauja irasa error ===', error);
+    console.warn('error ===', error.message);
+    return res.status(400).json({ error: 'something went wrong' });
+  }
 
-  // sukurimo rezultato tipas yra ResultSetHeader
-  let rez: ResultSetHeader;
-
-  res.json(argArr);
+  // // sukurimo rezultato tipas yra ResultSetHeader
+  // let rez: ResultSetHeader;
+  // res.json(rows);
+  res.json({ id: rows.insertId, ...req.body } as TripObjType);
 });
 
-// - DELETE /trips/:id - istrinti irasa
-//
+// - DELETE /trips/:id - istrinti irasa pakeiciant is_deleted i 1
+
+tripsRouter.delete('/:tripId', async (req, res) => {
+  const currentId = req.params.tripId;
+
+  const sql = `UPDATE trips SET is_deleted=1 WHERE id=?`;
+
+  const [rows, error] = (await dbQueryWithData(sql, [currentId])) as [ResultSetHeader, Error];
+
+  if (error) {
+    console.warn('istrinti irasa pakeiciant is_deleted i 1 error ===', error);
+    console.warn('error ===', error.message);
+    return res.status(400).json({ error: 'something went wrong' });
+  }
+
+  if (rows.affectedRows === 0) {
+    console.log('no rows');
+    return res.status(404).json({ msg: `trip with id: '${currentId}' was not found` });
+  }
+
+  res.json({ msg: `trip with id: '${currentId}' was deleted` });
+});
 
 export default tripsRouter;
