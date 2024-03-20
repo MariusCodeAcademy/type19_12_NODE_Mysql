3ed81a8b66b3091aff6e54a1374bdf0e515b44b1
MariusMokymai <testams752@gmail.com>
2024-03-20 11:08:29 +0200

    auth start

--- src/helpers/dbModel.sql
+++ src/helpers/dbModel.sql
@@ -1,3 +1,10 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 -- lentele
 
 CREATE TABLE trips (
---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,6 @@
+// sukursim authRouter
+
+// POST - /auth/register - gausim email ir password
+// ir israsysim i users lentele
+
+// sukurti useri is fron end
