e731ff426ffdac6671b662b21c113ab00b985378
MariusMokymai <testams752@gmail.com>
2024-03-27 09:58:19 +0200

    update

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dbQueryWithData from '../helpers/helper.js';
 import { UserObjType } from '../helpers/types.js';
+import { ResultSetHeader } from 'mysql2';
 
 const usersRouter = express.Router();
 
@@ -31,5 +32,21 @@ usersRouter.get('/:userId', async (req, res) => {
 // atnaujinti username
 // PUT/PATCH - /users/update/name/:userId - atnaujina vartotojo varda kurio id yra userId
 // updatedName request body
+usersRouter.put('/update/name/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const { updatedName } = req.body;
+
+  const sql = 'UPDATE `users` SET name = ? WHERE id = ?';
+
+  const [result, msqlErr] = await dbQueryWithData<ResultSetHeader>(sql, [updatedName, userId]);
+
+  if (msqlErr) {
+    console.warn('msqlErr ===', msqlErr);
+    return res.status(500).json('bad thing happen');
+  }
+
+  res.status(200).json({ success: true, msg: 'User name was updated' });
+  // res.status(200).end();
+});
 
 export default usersRouter;
